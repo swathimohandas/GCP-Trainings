171a231b9bdf400fe70e118751d542d91fb62aa3
swathimohandas <61185675+swathimohandas@users.noreply.github.com>
2020-12-02 19:11:45 +0530

    Add files via upload

---
+++ CICD_Fundamentals_Assignment/Assignment2_codes/cprogram.c
@@ -0,0 +1,7 @@
+#include<stdio.sh>
+int main(){
+printf("Hello from C Program");
+printf("Hello from DEV");
+printf("Hello from QA");
+return 0;
+}
---
+++ CICD_Fundamentals_Assignment/Assignment2_codes/hellojava.java
@@ -0,0 +1,5 @@
+class hellojava{
+public static void main(String args[]){
+System.out.Println("Hello from Java Program");
+ }
+}
